485998335934b3382de531f99c0639ce0b52a89a
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-09-03 02:30:04 +0300

    prepare routes

---
+++ internal/organizer/controller.go
@@ -0,0 +1,20 @@
+package organizer
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Controller struct {
+}
+
+func NewController() *Controller {
+	return &Controller{}
+}
+
+func (c *Controller) GolangCompaniesUkraine(ctx *gin.Context) {
+
+}
+
+func (c *Controller) GolangCompanies(ctx *gin.Context) {
+
+}
