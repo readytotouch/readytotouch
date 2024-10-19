c05b76138e384e1e7314e7a33bc7166d6b7ca141
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-10-19 03:02:17 +0300

    move template

--- internal/organizer/db/companies_part1.go
+++ internal/organizer/db/companies_part1.go
@@ -10155,41 +10155,5 @@ func companiesPart1() []domain.CompanyProfile {
 				domain.Czechia,
 			},
 		},
-
-		// Template
-		//{
-		//	ID:   0,  // system
-		//	Type: "", // system
-		//	Name: "",
-		//	URL:  "",
-		//	LinkedInProfile: domain.LinkedInProfile{
-		//		ID:    0,
-		//		Alias: "",
-		//		Name:  "",
-		//	},
-		//	GitHubProfile: domain.GitHubProfile{
-		//		Login:             "",
-		//		GoRepositoryCount: 0,
-		//	},
-		//	GlassdoorProfile: domain.GlassdoorProfile{
-		//		OverviewURL: "",
-		//		ReviewsURL:  "",
-		//	},
-		//	OttaProfileSlug:   "",
-		//	YouTubeChannelURL: "",
-		//	GoMainLanguage:    false,
-		//	Vacancies: domain.Vacancies{
-		//		domain.Go:      []string{},
-		//		domain.Rust:    nil,
-		//		domain.Zig:     nil,
-		//		domain.Scala:   nil,
-		//		domain.Elixir:  nil,
-		//		domain.Clojure: nil,
-		//		domain.Haskell: nil,
-		//	},
-		//	ShortDescription:          "",
-		//	Industries:                []domain.Industry{},
-		//	HasEmployeesFromCountries: []domain.Country{},
-		//},
 	}
 }
--- internal/organizer/db/companies_part2.go
+++ internal/organizer/db/companies_part2.go
@@ -3,5 +3,41 @@ package db
 import "github.com/readytotouch/readytotouch/internal/domain"
 
 func companiesPart2() []domain.CompanyProfile {
-	return []domain.CompanyProfile{}
+	return []domain.CompanyProfile{
+		// Template
+		//{
+		//	ID:   0,  // system
+		//	Type: "", // system
+		//	Name: "",
+		//	URL:  "",
+		//	LinkedInProfile: domain.LinkedInProfile{
+		//		ID:    0,
+		//		Alias: "",
+		//		Name:  "",
+		//	},
+		//	GitHubProfile: domain.GitHubProfile{
+		//		Login:             "",
+		//		GoRepositoryCount: 0,
+		//	},
+		//	GlassdoorProfile: domain.GlassdoorProfile{
+		//		OverviewURL: "",
+		//		ReviewsURL:  "",
+		//	},
+		//	OttaProfileSlug:   "",
+		//	YouTubeChannelURL: "",
+		//	GoMainLanguage:    false,
+		//	Vacancies: domain.Vacancies{
+		//		domain.Go:      []string{},
+		//		domain.Rust:    nil,
+		//		domain.Zig:     nil,
+		//		domain.Scala:   nil,
+		//		domain.Elixir:  nil,
+		//		domain.Clojure: nil,
+		//		domain.Haskell: nil,
+		//	},
+		//	ShortDescription:          "",
+		//	Industries:                []domain.Industry{},
+		//	HasEmployeesFromCountries: []domain.Country{},
+		//},
+	}
 }
