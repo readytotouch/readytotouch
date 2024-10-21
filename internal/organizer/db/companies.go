1175918960cf75d1ef5edcbcf4098befa7b0a10f
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-10-21 21:27:31 +0300

    More companies October 2024 v6

--- internal/organizer/db/companies.go
+++ internal/organizer/db/companies.go
@@ -6,11 +6,13 @@ func Companies() []domain.CompanyProfile {
 	var (
 		part1  = companiesPart1()
 		part2  = companiesPart2()
-		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2))
+		part3  = companiesPart3()
+		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2)+len(part3))
 	)
 
 	result = append(result, part1...)
 	result = append(result, part2...)
+	result = append(result, part3...)
 
 	return result
 }
