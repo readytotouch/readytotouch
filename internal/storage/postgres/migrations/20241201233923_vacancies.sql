7bf5be055bb770f0e0f350552217b1c304e13aff
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-12-05 00:48:37 +0200

    hardcode company_id for now

--- internal/storage/postgres/migrations/20241201233923_vacancies.sql
+++ internal/storage/postgres/migrations/20241201233923_vacancies.sql
@@ -44,6 +44,14 @@ CREATE TABLE vacancy_view_daily_stats
     PRIMARY KEY (vacancy_id, created_at)
 );
 
+INSERT INTO vacancies (id, company_id, created_at, created_by)
+SELECT vacancy_id,
+       1 AS company_id, -- hardcode company_id for now
+       '2024-09-17 12:00:00',
+       1
+FROM GENERATE_SERIES(1, 1024) AS vacancy_id
+ON CONFLICT DO NOTHING;
+
 SELECT SETVAL('vacancies_id_seq', (SELECT COALESCE(MAX(id), 1) FROM vacancies));
 -- +goose StatementEnd
 
