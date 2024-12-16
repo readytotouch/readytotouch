76c755e441cd5701e588dab2b1bffb1e84786a06
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-12-16 22:20:43 +0200

    companiesPart4

--- internal/organizer/db/companies.go
+++ internal/organizer/db/companies.go
@@ -7,12 +7,14 @@ func Companies() []domain.CompanyProfile {
 		part1  = companiesPart1()
 		part2  = companiesPart2()
 		part3  = companiesPart3()
-		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2)+len(part3))
+		part4  = companiesPart4()
+		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2)+len(part3)+len(part4))
 	)
 
 	result = append(result, part1...)
 	result = append(result, part2...)
 	result = append(result, part3...)
+	result = append(result, part4...)
 
 	return result
 }
---
+++ internal/organizer/db/companies_part4.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"github.com/readytotouch/readytotouch/internal/domain"
+)
+
+func companiesPart4() []domain.CompanyProfile {
+	return []domain.CompanyProfile{
+
+		// Template short
+		//{
+		//	ID:      0,  // system
+		//	Type:    "", // system
+		//	Name:    "",
+		//	Website: "",
+		//	LinkedInProfile: domain.LinkedInProfile{
+		//		ID:       0,
+		//		Alias:    "",
+		//		Name:     "",
+		//		Verified: false,
+		//	},
+		//	Languages: domain.Languages{
+		//		domain.Go: {
+		//			GitHubRepositoriesCount: 0,
+		//			Vacancies: []domain.Vacancy{
+		//				{
+		//					Title:            "",
+		//					ShortDescription: "",
+		//					URL:              "",
+		//					Date:             mustDate(""),
+		//				},
+		//			},
+		//		},
+		//		domain.Rust:    {},
+		//		domain.Zig:     {},
+		//		domain.Scala:   {},
+		//		domain.Elixir:  {},
+		//		domain.Clojure: {},
+		//		domain.Haskell: {},
+		//	},
+		//},
+	}
+}
