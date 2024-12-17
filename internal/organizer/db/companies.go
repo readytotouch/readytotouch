6abde46c6e7cdcb2f02bac2d26c85600af6a7157
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-12-18 00:38:41 +0200

    companiesPart5

--- internal/organizer/db/companies.go
+++ internal/organizer/db/companies.go
@@ -8,13 +8,15 @@ func Companies() []domain.CompanyProfile {
 		part2  = companiesPart2()
 		part3  = companiesPart3()
 		part4  = companiesPart4()
-		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2)+len(part3)+len(part4))
+		part5  = companiesPart5()
+		result = make([]domain.CompanyProfile, 0, len(part1)+len(part2)+len(part3)+len(part4)+len(part5))
 	)
 
 	result = append(result, part1...)
 	result = append(result, part2...)
 	result = append(result, part3...)
 	result = append(result, part4...)
+	result = append(result, part5...)
 
 	return result
 }
