cfc47a2558155a4650b99f579dfa373f1e96c363
Yaroslav Podorvanov <63663261+YaroslavPodorvanov@users.noreply.github.com>
2025-01-07 23:38:54 +0200

    More companies January 2025 v002 (#89)

---
+++ internal/organizer/db/companies_part8.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"github.com/readytotouch/readytotouch/internal/domain"
+)
+
+func companiesPart8() []domain.CompanyProfile {
+	return []domain.CompanyProfile{
+
+		// Template short
+		//{
+		//	ID:      0,  // system
+		//	Type:    "", // system
+		//	Name:    "",
+		//	Website: "",
+		//	Careers: "",
+		//	About:   "",
+		//	LinkedInProfile: domain.LinkedInProfile{
+		//		ID:                0,
+		//		Alias:             "",
+		//		Name:              "",
+		//		Followers:         "",
+		//		Employees:         "",
+		//		AssociatedMembers: "",
+		//		Verified:          false,
+		//	},
+		//	GitHubProfile: domain.GitHubProfile{
+		//		Login:    "",
+		//		Verified: false,
+		//	},
+		//	Languages: domain.Languages{
+		//		domain.Go: {
+		//			GitHubRepositoriesCount: 0,
+		//			Vacancies: []domain.Vacancy{
+		//				{
+		//					Title:                "",
+		//					ShortDescription:     "",
+		//					SwitchingOpportunity: "",
+		//					URL:                  "",
+		//					Date:                 mustDate(""),
+		//					WithSalary:           false,
+		//					Remote:               false,
+		//				},
+		//			},
+		//		},
+		//		domain.Rust:    {},
+		//		domain.Zig:     {},
+		//		domain.Scala:   {},
+		//		domain.Elixir:  {},
+		//		domain.Clojure: {},
+		//		domain.Haskell: {},
+		//	},
+		//	ShortDescription: "",
+		//},
+	}
+}
