3b993516e3b1ea0f4823c54766e7ddcbf7907453
Mykhailo Upir <mykhailo.upir@gmail.com>
2025-01-11 00:17:37 +0100

    More companies LinkedIn January 2025 v003 updt

--- internal/organizer/db/companies_part8.go
+++ internal/organizer/db/companies_part8.go
@@ -276,6 +276,232 @@ func companiesPart8() []domain.CompanyProfile {
 			},
 			ShortDescription: "American video game development company",
 		},
+
+		{
+			ID:      0,  // system
+			Type:    "", // system
+			Name:    "BetterMe",
+			Website: "https://betterme.world/",
+			Careers: "https://betterme.world/career",
+			About:   "",
+			LinkedInProfile: domain.LinkedInProfile{
+				ID:                18550246,
+				Alias:             "betterme-company",
+				Name:              "BetterMe",
+				Followers:         "12K",
+				Employees:         "201-500",
+				AssociatedMembers: "482",
+				Verified:          true,
+			},
+			GitHubProfile: domain.GitHubProfile{
+				Login:    "betterme-dev",
+				Verified: false,
+			},
+			Languages: domain.Languages{
+				domain.Go: {
+					GitHubRepositoriesCount: 0,
+					Vacancies: []domain.Vacancy{
+						{
+							Title:                "Backend Engineer (Go + PHP)",
+							ShortDescription:     "Design and implement backend features from conception through to deployment, ensuring they meet strategic business objectives",
+							SwitchingOpportunity: "",
+							URL:                  "https://www.linkedin.com/jobs/view/4121587727/",
+							Date:                 mustDate("2025-01-10"),
+							WithSalary:           false,
+							Remote:               false,
+						},
+					},
+				},
+				domain.Rust:    {},
+				domain.Zig:     {},
+				domain.Scala:   {},
+				domain.Elixir:  {},
+				domain.Clojure: {},
+				domain.Haskell: {},
+			},
+			ShortDescription: "Digital fitness company",
+		},
+
+		{
+			ID:      0,  // system
+			Type:    "", // system
+			Name:    "SPE Solution",
+			Website: "https://www.spesolution.com/",
+			Careers: "https://www.spesolution.com/career",
+			About:   "",
+			LinkedInProfile: domain.LinkedInProfile{
+				ID:                30205768,
+				Alias:             "spe-solution",
+				Name:              "SPE Solution",
+				Followers:         "7K",
+				Employees:         "201-500",
+				AssociatedMembers: "190",
+				Verified:          true,
+			},
+			GitHubProfile: domain.GitHubProfile{
+				Login:    "",
+				Verified: false,
+			},
+			Languages: domain.Languages{
+				domain.Go: {
+					GitHubRepositoriesCount: 0,
+					Vacancies: []domain.Vacancy{
+						{
+							Title:                "Backend Developer — Golang",
+							ShortDescription:     "At least 3-4 years as a Golang Developer or 5 years as a Backend Developer.",
+							SwitchingOpportunity: "",
+							URL:                  "https://www.linkedin.com/jobs/view/4120414743/",
+							Date:                 mustDate("2025-01-10"),
+							WithSalary:           false,
+							Remote:               false,
+						},
+					},
+				},
+				domain.Rust:    {},
+				domain.Zig:     {},
+				domain.Scala:   {},
+				domain.Elixir:  {},
+				domain.Clojure: {},
+				domain.Haskell: {},
+			},
+			ShortDescription: "Indonesian fintech solution company",
+		},
+
+		{
+			ID:      0,  // system
+			Type:    "", // system
+			Name:    "Quantum Systems",
+			Website: "https://quantum-systems.com/",
+			Careers: "https://career.quantum-systems.com/",
+			About:   "",
+			LinkedInProfile: domain.LinkedInProfile{
+				ID:                25007323,
+				Alias:             "quantum-systems-gmbh",
+				Name:              "Quantum Systems",
+				Followers:         "37K",
+				Employees:         "201-500",
+				AssociatedMembers: "284",
+				Verified:          true,
+			},
+			GitHubProfile: domain.GitHubProfile{
+				Login:    "",
+				Verified: false,
+			},
+			Languages: domain.Languages{
+				domain.Go: {},
+				domain.Rust: {
+					GitHubRepositoriesCount: 0,
+					Vacancies: []domain.Vacancy{
+						{
+							Title:                "Rust Developer",
+							ShortDescription:     "5+ years of professional experience in backend development with a strong focus on real-time systems. ",
+							SwitchingOpportunity: "",
+							URL:                  "https://www.linkedin.com/jobs/view/4119548956/",
+							Date:                 mustDate("2025-01-10"),
+							WithSalary:           false,
+							Remote:               false, // Mobile working
+						},
+					},
+				},
+				domain.Zig:     {},
+				domain.Scala:   {},
+				domain.Elixir:  {},
+				domain.Clojure: {},
+				domain.Haskell: {},
+			},
+			ShortDescription: "German technology company",
+		},
+
+		{
+			ID:      0,  // system
+			Type:    "", // system
+			Name:    "General Dynamics Mission Systems",
+			Website: "https://gdmissionsystems.com/",
+			Careers: "https://gdmissionsystems.com/careers",
+			About:   "",
+			LinkedInProfile: domain.LinkedInProfile{
+				ID:                17954388,
+				Alias:             "gdms",
+				Name:              "General Dynamics Mission Systems",
+				Followers:         "165K",
+				Employees:         "10K",
+				AssociatedMembers: "8,822",
+				Verified:          true,
+			},
+			GitHubProfile: domain.GitHubProfile{
+				Login:    "",
+				Verified: false,
+			},
+			Languages: domain.Languages{
+				domain.Go: {},
+				domain.Rust: {
+					GitHubRepositoriesCount: 0,
+					Vacancies: []domain.Vacancy{
+						{
+							Title:                "RUST Embedded Software Engineer",
+							ShortDescription:     "Long development lifecycle and product lifespans, requiring maintenance and regular technology refresh.",
+							SwitchingOpportunity: "",
+							URL:                  "https://www.linkedin.com/jobs/view/4118966168/",
+							Date:                 mustDate("2025-01-09"),
+							WithSalary:           false,
+							Remote:               false,
+						},
+					},
+				},
+				domain.Zig:     {},
+				domain.Scala:   {},
+				domain.Elixir:  {},
+				domain.Clojure: {},
+				domain.Haskell: {},
+			},
+			ShortDescription: "Integrates secure communication and information systems and technology.",
+		},
+
+		{
+			ID:      0,  // system
+			Type:    "", // system
+			Name:    "Antmicro",
+			Website: "https://antmicro.com/",
+			Careers: "https://careers.antmicro.com/index.html",
+			About:   "",
+			LinkedInProfile: domain.LinkedInProfile{
+				ID:                3267482,
+				Alias:             "antmicro-ltd",
+				Name:              "Antmicro",
+				Followers:         "6K",
+				Employees:         "51-200",
+				AssociatedMembers: "83",
+				Verified:          true,
+			},
+			GitHubProfile: domain.GitHubProfile{
+				Login:    "antmicro",
+				Verified: true,
+			},
+			Languages: domain.Languages{
+				domain.Go: {
+					GitHubRepositoriesCount: 0,
+					Vacancies: []domain.Vacancy{
+						{
+							Title:                "Open Source C / Rust Engineer",
+							ShortDescription:     "You will have to solve complex problems that might include working with assembly, code generation, code translation, interoperability between higher and lower level languages and portability between different OSes.",
+							SwitchingOpportunity: "",
+							URL:                  "https://www.linkedin.com/jobs/view/3706084909/",
+							Date:                 mustDate("2025-01-09"),
+							WithSalary:           false,
+							Remote:               false,
+						},
+					},
+				},
+				domain.Rust:    {},
+				domain.Zig:     {},
+				domain.Scala:   {},
+				domain.Elixir:  {},
+				domain.Clojure: {},
+				domain.Haskell: {},
+			},
+			ShortDescription: "Sweden software-driven tech company",
+		},
+
 		// Template short
 		//{
 		//	ID:      0,  // system
