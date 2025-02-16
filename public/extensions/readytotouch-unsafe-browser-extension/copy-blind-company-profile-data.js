93815848ffca6eab3fa9419446dada0944759011
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-02-16 21:48:57 +0200

    wip

--- public/extensions/readytotouch-unsafe-browser-extension/copy-blind-company-profile-data.js
+++ public/extensions/readytotouch-unsafe-browser-extension/copy-blind-company-profile-data.js
@@ -4,10 +4,44 @@ console.log("Blind company profile data copy extension loaded");
 document.body.addEventListener("keydown", (event) => {
     // Y is for English, Н is for Ukrainian
     if (event.ctrlKey && event.shiftKey && (event.key === "Y" || event.key === "Н")) {
-        const goBlindProfileColumns = ``
+        const goBlindProfileColumns = `				Alias:       "${parseVanityName(window.location.href)}",
+				Employees:   "",
+				Salary:      "",
+				Reviews:     "",
+				ReviewsRate: "",`
 
         navigator.clipboard.writeText(goBlindProfileColumns)
             .then(() => console.log("Page info copied to clipboard:", goBlindProfileColumns))
             .catch((err) => console.error("Failed to copy page info:", err));
     }
 });
+
+function parseVanityName(url) {
+    // Your errors = your pain
+    const error = "Expected URL like https://www.teamblind.com/company/company-name/";
+
+    let parsedUrl = null;
+
+    try {
+        parsedUrl = new URL(url);
+    } catch (e) {
+        alert(error);
+
+        return "";
+    }
+
+    const prefix = "/company/";
+
+    if (parsedUrl.pathname.indexOf(prefix) === -1) {
+        alert(error);
+
+        return "";
+    }
+
+    const end = parsedUrl.pathname.indexOf("/", prefix.length);
+    if (end === -1) {
+        return parsedUrl.pathname.substring(prefix.length);
+    }
+
+    return parsedUrl.pathname.substring(prefix.length, end);
+}
