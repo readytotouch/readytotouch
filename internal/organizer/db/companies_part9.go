c2b161a4f5a10a26b98cf9b4632a8a52c1258a7b
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-03-06 21:37:14 +0200

    new companies part

---
+++ internal/organizer/db/companies_part9.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"github.com/readytotouch/readytotouch/internal/domain"
+)
+
+func companiesPart9() []domain.CompanyProfile {
+	return []domain.CompanyProfile{
+
+		// Template
+		//{
+		//	ID:      0,  // system
+		//	Type:    "", // system
+		//	Name:    "",
+		//	Website: "",
+		//	Careers: "",
+		//	About:   "",
+		//	Blog:    "",
+		//	LinkedInProfile: domain.LinkedInProfile{
+		//		ID:                0,
+		//		Alias:             "",
+		//		Name:              "",
+		//		Followers:         "",
+		//		Employees:         "",
+		//		AssociatedMembers: "",
+		//		Verified:          false,
+		//	},
+		//	GitHubProfile: domain.GitHubProfile{
+		//		Login:    "",
+		//		Verified: false,
+		//	},
+		//	BlindProfile: domain.BlindProfile{
+		//		Alias: "",
+		//	},
+		//	LevelsFyiProfile: domain.LevelsFyiProfile{
+		//		Alias: "",
+		//	},
+		//	GlassdoorProfile: domain.GlassdoorProfile{
+		//		OverviewURL: "",
+		//	},
+		//	IndeedProfile: domain.IndeedProfile{
+		//		Alias: "",
+		//	},
+		//	Languages: domain.Languages{
+		//		domain.Go: {
+		//			GitHubRepositoriesCount: 0,
+		//			Vacancies: []domain.Vacancy{
+		//				{
+		//					Title:                "",
+		//					ShortDescription:     "",
+		//					SwitchingOpportunity: "",
+		//					URL:                  "",
+		//					Date:                 mustDate(""),
+		//					WithSalary:           false,
+		//					Remote:               false,
+		//				},
+		//			},
+		//		},
+		//		domain.Rust:    {},
+		//		domain.Zig:     {},
+		//		domain.Scala:   {},
+		//		domain.Elixir:  {},
+		//		domain.Clojure: {},
+		//		domain.Haskell: {},
+		//	},
+		//	ShortDescription: "",
+		//},
+	}
+}
