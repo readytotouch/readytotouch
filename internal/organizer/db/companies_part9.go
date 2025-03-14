8403e25444d6c5a5596b7786fddbb57996afdc50
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-03-14 23:33:39 +0200

    wip

--- internal/organizer/db/companies_part9.go
+++ internal/organizer/db/companies_part9.go
@@ -3757,19 +3757,20 @@ func companiesPart9() []domain.CompanyProfile {
 		{
 			ID:      0,  // system
 			Type:    "", // system
-			Name:    "",
-			Website: "",
+			Name:    "CoinGate",
+			Website: "https://coingate.com/",
 			Careers: "",
 			About:   "",
 			Blog:    "",
 			LinkedInProfile: domain.LinkedInProfile{
-				ID:                0,
-				Alias:             "",
-				Name:              "",
-				Followers:         "",
-				Employees:         "",
-				AssociatedMembers: "",
-				Verified:          false,
+				ID:                10586817,
+				IDs:               nil,
+				Alias:             "coingate",
+				Name:              "CoinGate",
+				Followers:         "9K",
+				Employees:         "51-200",
+				AssociatedMembers: "81",
+				Verified:          true,
 			},
 			GitHubProfile: domain.GitHubProfile{
 				Login:    "",
@@ -3792,11 +3793,11 @@ func companiesPart9() []domain.CompanyProfile {
 					GitHubRepositoriesCount: 0,
 					Vacancies: []domain.Vacancy{
 						{
-							Title:                "",
+							Title:                "Senior Golang Engineer",
 							ShortDescription:     "",
 							SwitchingOpportunity: "",
-							URL:                  "",
-							Date:                 mustDate(""),
+							URL:                  "https://www.linkedin.com/jobs/view/4184624827/",
+							Date:                 mustDate("2025-03-14"),
 							WithSalary:           false,
 							Remote:               false,
 						},
@@ -3810,6 +3811,7 @@ func companiesPart9() []domain.CompanyProfile {
 				domain.Haskell: {},
 			},
 			ShortDescription: "",
+			Ignore:           true, // Crypto
 		},
 		{
 			ID:      0,  // system
