7c4c251f64a36def1c29c38a700e2966ffa23b99
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-03-20 03:37:38 +0200

    wip

--- internal/organizer/db/companies_part9.go
+++ internal/organizer/db/companies_part9.go
@@ -6381,19 +6381,20 @@ func companiesPart9() []domain.CompanyProfile {
 		{
 			ID:      0,  // system
 			Type:    "", // system
-			Name:    "",
-			Website: "",
+			Name:    "Veact GmbH",
+			Website: "https://veact.net/",
 			Careers: "",
 			About:   "",
 			Blog:    "",
 			LinkedInProfile: domain.LinkedInProfile{
-				ID:                0,
-				Alias:             "",
-				Name:              "",
-				Followers:         "",
-				Employees:         "",
-				AssociatedMembers: "",
-				Verified:          false,
+				ID:                1412127,
+				IDs:               nil,
+				Alias:             "veact",
+				Name:              "Veact GmbH",
+				Followers:         "2K",
+				Employees:         "51-200",
+				AssociatedMembers: "111",
+				Verified:          true,
 			},
 			GitHubProfile: domain.GitHubProfile{
 				Login:    "",
@@ -6412,28 +6413,28 @@ func companiesPart9() []domain.CompanyProfile {
 				Alias: "",
 			},
 			Languages: domain.Languages{
-				domain.Go: {
+				domain.Go:   {},
+				domain.Rust: {},
+				domain.Zig:  {},
+				domain.Scala: {
 					GitHubRepositoriesCount: 0,
 					Vacancies: []domain.Vacancy{
 						{
-							Title:                "",
+							Title:                "Scala Developer",
 							ShortDescription:     "",
 							SwitchingOpportunity: "",
-							URL:                  "",
-							Date:                 mustDate(""),
+							URL:                  "https://www.linkedin.com/jobs/view/4182628362/",
+							Date:                 mustDate("2025-03-15"),
 							WithSalary:           false,
 							Remote:               false,
 						},
 					},
 				},
-				domain.Rust:    {},
-				domain.Zig:     {},
-				domain.Scala:   {},
 				domain.Elixir:  {},
 				domain.Clojure: {},
 				domain.Haskell: {},
 			},
-			ShortDescription: "",
+			ShortDescription: "Veact is developing applications, processes and products to improve sales success in the car trade market using data-based marketing",
 		},
 		{
 			ID:      0,  // system
