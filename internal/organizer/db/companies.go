73656213efe4360679b815a3d1d04cf5b30a0159
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-04-01 23:50:31 +0300

    Review

--- internal/organizer/db/companies.go
+++ internal/organizer/db/companies.go
@@ -13,6 +13,9 @@ func Companies() []domain.CompanyProfile {
 		part7  = companiesPart7()
 		part8  = companiesPart8()
 		part9  = companiesPart9()
+		part10 = companiesPart10()
+		part11 = companiesPart11()
+		part12 = companiesPart12()
 		result = make([]domain.CompanyProfile, 0, len(part1)+
 			len(part2)+
 			len(part3)+
@@ -21,7 +24,10 @@ func Companies() []domain.CompanyProfile {
 			len(part6)+
 			len(part7)+
 			len(part8)+
-			len(part9))
+			len(part9)+
+			len(part10)+
+			len(part11)+
+			len(part12))
 	)
 
 	result = append(result, part1...)
@@ -33,6 +39,9 @@ func Companies() []domain.CompanyProfile {
 	result = append(result, part7...)
 	result = append(result, part8...)
 	result = append(result, part9...)
+	result = append(result, part10...)
+	result = append(result, part11...)
+	result = append(result, part12...)
 
 	return result
 }
