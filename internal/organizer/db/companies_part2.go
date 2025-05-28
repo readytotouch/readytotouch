3020e62f662b268584ea4b52110695c3a427b303
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-05-29 02:21:50 +0300

    Elixir companies

--- internal/organizer/db/companies_part2.go
+++ internal/organizer/db/companies_part2.go
@@ -10762,7 +10762,7 @@ func companiesPart2() []domain.CompanyProfile {
 							ShortDescription:     "",
 							SwitchingOpportunity: "",
 							URL:                  "https://www.linkedin.com/jobs/view/4187506903/",
-							Date:                 mustDate("2025-05-03"), // mustDate("2025-04-12"),
+							Date:                 mustDate("2025-05-26"), // mustDate("2025-05-03"), // mustDate("2025-04-12"),
 							WithSalary:           false,
 							Remote:               false,
 						},
