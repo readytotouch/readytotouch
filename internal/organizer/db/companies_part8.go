2b3fc2d4f491ea8866f31fd988b7e0999b217d08
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-06-09 01:06:03 +0300

    Elixir companies

--- internal/organizer/db/companies_part8.go
+++ internal/organizer/db/companies_part8.go
@@ -4980,7 +4980,7 @@ func companiesPart8() []domain.CompanyProfile {
 							ShortDescription:     "",
 							SwitchingOpportunity: "",
 							URL:                  "https://www.linkedin.com/jobs/view/4137064133/",
-							Date:                 mustDate("2025-05-15"), //  mustDate("2025-04-24"), //  mustDate("2025-04-02"), // mustDate("2025-03-12"), // mustDate("2025-02-18"), // mustDate("2025-01-30"),
+							Date:                 mustDate("2025-06-07"), // mustDate("2025-05-15"), //  mustDate("2025-04-24"), //  mustDate("2025-04-02"), // mustDate("2025-03-12"), // mustDate("2025-02-18"), // mustDate("2025-01-30"),
 							WithSalary:           true,                   // $188.000 - $258.000 per year
 							Remote:               true,
 						},
