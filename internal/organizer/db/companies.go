b072da6e7da8f0fa5cc01246a520af0e0d358bab
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-06-15 03:19:43 +0300

    Remove debug

--- internal/organizer/db/companies.go
+++ internal/organizer/db/companies.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"slices"
 
 	"github.com/readytotouch/readytotouch/internal/domain"
@@ -13,141 +12,73 @@ var (
 
 func Companies() []domain.CompanyProfile {
 	var (
-		part01             = companies01Shard()
-		part02             = companies02Shard()
-		part03             = companies03Shard()
-		part04             = companies04Shard()
-		part05             = companies05Shard()
-		part06             = companies06Shard()
-		part07             = companies07Shard()
-		part08             = companies08Shard()
-		part09             = companies09Shard()
-		part10             = companies10Shard()
-		part11             = companies11Shard()
-		part12             = companies12Shard()
-		part13             = companies13Shard()
-		part14             = companies14Shard()
-		part15             = companies15Shard()
-		part16             = companies16Shard()
-		part17             = companies17Shard()
-		part18             = companies18Shard()
-		part19             = companies19Shard()
-		part20             = companies20Shard()
+		shard01            = companies01Shard()
+		shard02            = companies02Shard()
+		shard03            = companies03Shard()
+		shard04            = companies04Shard()
+		shard05            = companies05Shard()
+		shard06            = companies06Shard()
+		shard07            = companies07Shard()
+		shard08            = companies08Shard()
+		shard09            = companies09Shard()
+		shard10            = companies10Shard()
+		shard11            = companies11Shard()
+		shard12            = companies12Shard()
+		shard13            = companies13Shard()
+		shard14            = companies14Shard()
+		shard15            = companies15Shard()
+		shard16            = companies16Shard()
+		shard17            = companies17Shard()
+		shard18            = companies18Shard()
+		shard19            = companies19Shard()
+		shard20            = companies20Shard()
 		outsourceCompanies = companiesOutsourceCompanies()
 		recruitingAgencies = companiesRecruitingAgencies()
-		result             = make([]domain.CompanyProfile, 0, len(part01)+
-			len(part02)+
-			len(part03)+
-			len(part04)+
-			len(part05)+
-			len(part06)+
-			len(part07)+
-			len(part08)+
-			len(part09)+
-			len(part10)+
-			len(part11)+
-			len(part12)+
-			len(part13)+
-			len(part14)+
-			len(part15)+
-			len(part16)+
-			len(part17)+
-			len(part18)+
-			len(part19)+
-			len(part20)+
+		result             = make([]domain.CompanyProfile, 0, len(shard01)+
+			len(shard02)+
+			len(shard03)+
+			len(shard04)+
+			len(shard05)+
+			len(shard06)+
+			len(shard07)+
+			len(shard08)+
+			len(shard09)+
+			len(shard10)+
+			len(shard11)+
+			len(shard12)+
+			len(shard13)+
+			len(shard14)+
+			len(shard15)+
+			len(shard16)+
+			len(shard17)+
+			len(shard18)+
+			len(shard19)+
+			len(shard20)+
 			len(outsourceCompanies)+
 			len(recruitingAgencies),
 		)
 	)
 
-	const debug = true
-
-	if debug {
-		fmt.Println(
-			"part01", len(part01), "\n",
-			"part02", len(part02), "\n",
-			"part03", len(part03), "\n",
-			"part04", len(part04), "\n",
-			"part05", len(part05), "\n",
-			"part06", len(part06), "\n",
-			"part07", len(part07), "\n",
-			"part08", len(part08), "\n",
-			"part09", len(part09), "\n",
-			"part10", len(part10), "\n",
-			"part11", len(part11), "\n",
-			"part12", len(part12), "\n",
-			"part13", len(part13), "\n",
-			"part14", len(part14), "\n",
-			"part15", len(part15), "\n",
-			"part16", len(part16), "\n",
-			"part17", len(part17), "\n",
-			"part18", len(part18), "\n",
-			"part19", len(part19), "\n",
-			"part20", len(part20), "\n",
-			len(part01)+
-				len(part02)+
-				len(part03)+
-				len(part04)+
-				len(part05)+
-				len(part06)+
-				len(part07)+
-				len(part08)+
-				len(part09)+
-				len(part10)+
-				len(part11)+
-				len(part12)+
-				len(part13)+
-				len(part14)+
-				len(part15)+
-				len(part16)+
-				len(part17)+
-				len(part18)+
-				len(part19)+
-				len(part20),
-			(len(part01)+
-				len(part02)+
-				len(part03)+
-				len(part04)+
-				len(part05)+
-				len(part06)+
-				len(part07)+
-				len(part08)+
-				len(part09)+
-				len(part10)+
-				len(part11)+
-				len(part12)+
-				len(part13)+
-				len(part14)+
-				len(part15)+
-				len(part16)+
-				len(part17)+
-				len(part18)+
-				len(part19)+
-				len(part20)+
-				0)/64,
-		)
-	}
-
-	result = append(result, part01...)
-	result = append(result, part02...)
-	result = append(result, part03...)
-	result = append(result, part04...)
-	result = append(result, part05...)
-	result = append(result, part06...)
-	result = append(result, part07...)
-	result = append(result, part08...)
-	result = append(result, part09...)
-	result = append(result, part10...)
-	result = append(result, part11...)
-	result = append(result, part12...)
-	result = append(result, part13...)
-	result = append(result, part14...)
-	result = append(result, part15...)
-	result = append(result, part16...)
-	result = append(result, part17...)
-	result = append(result, part18...)
-	result = append(result, part19...)
-	result = append(result, part20...)
+	result = append(result, shard01...)
+	result = append(result, shard02...)
+	result = append(result, shard03...)
+	result = append(result, shard04...)
+	result = append(result, shard05...)
+	result = append(result, shard06...)
+	result = append(result, shard07...)
+	result = append(result, shard08...)
+	result = append(result, shard09...)
+	result = append(result, shard10...)
+	result = append(result, shard11...)
+	result = append(result, shard12...)
+	result = append(result, shard13...)
+	result = append(result, shard14...)
+	result = append(result, shard15...)
+	result = append(result, shard16...)
+	result = append(result, shard17...)
+	result = append(result, shard18...)
+	result = append(result, shard19...)
+	result = append(result, shard20...)
 	result = append(result, outsourceCompanies...)
 	result = append(result, recruitingAgencies...)
 
