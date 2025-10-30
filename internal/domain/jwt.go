f3bb3d65d87c1ba7e6e9c7f9379bcfdd18cd96cb
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-10-30 04:22:55 +0300

    review

--- internal/domain/jwt.go
+++ internal/domain/jwt.go
@@ -34,12 +34,6 @@ func ContextGetUser(ctx *gin.Context) (*JwtUser, bool) {
 }
 
 func ContextGetUserID(ctx *gin.Context) int64 {
-	// @TODO: remove before merge
-	const debug = true
-	if debug {
-		return 1
-	}
-
 	user, ok := ContextGetUser(ctx)
 	if ok {
 		return user.ID
