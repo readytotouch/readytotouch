923d9812a77ffbf7b422ebf770b54bd41fc28f3f
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2026-03-18 01:28:58 +0300

    Haskell companies

--- internal/organizer/db/companies_22_shard.go
+++ internal/organizer/db/companies_22_shard.go
@@ -3080,7 +3080,7 @@ func companies22Shard() []domain.CompanyProfile {
 							SwitchingOpportunity: "",
 							URL:                  "https://www.linkedin.com/jobs/view/4306246217/",
 							Location:             "Ghent, Flemish Region, Belgium",
-							Date:                 mustDate("2026-02-23", "2026-02-02", "2025-12-21", "2025-11-29"),
+							Date:                 mustDate("2026-03-16"), // mustDate("2026-02-23", "2026-02-02", "2025-12-21", "2025-11-29"),
 							WithSalary:           false,
 							Remote:               false,
 						},
