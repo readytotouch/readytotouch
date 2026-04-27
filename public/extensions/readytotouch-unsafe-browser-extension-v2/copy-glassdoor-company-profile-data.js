d485fa80d182792c19367962be212ad50ac7fa25
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2026-04-28 02:39:19 +0300

    review

--- public/extensions/readytotouch-unsafe-browser-extension-v2/copy-glassdoor-company-profile-data.js
+++ public/extensions/readytotouch-unsafe-browser-extension-v2/copy-glassdoor-company-profile-data.js
@@ -4,12 +4,11 @@ console.log("[RTT] Glassdoor company profile copy loaded");
 
 // Ctrl+Shift+Y / Ctrl+Shift+Н
 document.body.addEventListener("keydown", (event) => {
-    if (!event.ctrlKey || !event.shiftKey) return;
-    if (event.key !== "Y" && event.key !== "Н") return;
+    if (event.ctrlKey && event.shiftKey && (event.key === "Y" || event.key === "Н")) {
 
-    const out = `\t\t\t\tOverviewURL: "${qs('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Overview/"]')?.href ?? ""}",
-\t\t\t\tReviewsURL:  "${qs('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Reviews/"]')?.href ?? ""}",
-\t\t\t\tJobsURL:     "${qs('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Jobs/"]')?.href ?? ""}",
+        const out = `\t\t\t\tOverviewURL: "${document.querySelector('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Overview/"]')?.href ?? ""}",
+\t\t\t\tReviewsURL:  "${document.querySelector('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Reviews/"]')?.href ?? ""}",
+\t\t\t\tJobsURL:     "${document.querySelector('div[class^="EmployerInfo_employerInfoWrapper"] a[href^="/Jobs/"]')?.href ?? ""}",
 \t\t\t\tJobs:        "", // Becomes empty in the new design version
 \t\t\t\tReviews:     "", // Becomes empty in the new design version
 \t\t\t\tSalaries:    "", // Becomes empty in the new design version
@@ -17,19 +16,23 @@ document.body.addEventListener("keydown", (event) => {
 \t\t\t\tVerified:    ${isEngagedEmployer() ? "true" : "false"},
 \t\t\t\tDate:        mustDate("${RTT.today()}"),`;
 
-    RTT.copyToClipboard(out);
+        RTT.copyToClipboard(out);
+    }
 });
 
-function qs(selector) {
-    return document.querySelector(selector);
-}
-
 function isEngagedEmployer() {
-    return Array.from(document.querySelectorAll('div[class^="EmployerInfo_employerInfoEngagementStatus"]'))
-        .some(el => el.textContent.trim() === "Engaged Employer");
+    const $elements = document.querySelectorAll('div[class^="EmployerInfo_employerInfoEngagementStatus"]');
+
+    return Array
+        .from($elements)
+        .some($element => $element.textContent.trim() === "Engaged Employer");
 }
 
 function getEmployerRating() {
-    const el = document.querySelector('span[class^="employer-overview_employerOverviewRating"]');
-    return el ? el.childNodes[0].textContent.trim() : "";
+    const $rating = document.querySelector(`div[class^="CompanyOverview_overallRating"]`);
+    if ($rating) {
+        return $rating.childNodes[0].textContent.trim();
+    }
+
+    return "";
 }
